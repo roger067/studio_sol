9859145fad1d09435c76ff4182c1566e7956e246
roger067 <rosestolato.rogerio@gmail.com>
2023-11-06 18:50:56 -0300

    create global style

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import router from './routes/router';
+import GlobalStyle from './styles/globalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
