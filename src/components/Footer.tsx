be898cbf400bf36fe199b0336e1807fa4e042fb6
roger067 <rosestolato.rogerio@gmail.com>
2023-11-08 02:18:33 -0300

    feat: style footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,32 @@
 import styled from 'styled-components';
-import { Flex } from '../ui';
 
-const Footer = () => <FooterTag as="footer"></FooterTag>;
+import { COLORS, Container, Flex, Text } from '../ui';
 
-const FooterTag = styled(Flex)``;
+import { ReactComponent as Logo } from '../assets/logo-white.svg';
+
+const Footer = () => (
+  <FooterTag>
+    <Container>
+      <Flex justifyContent="space-between" alignItems="center">
+        <Logo />
+        <Flex flexDirection="column">
+          <Text textAlign="right" color={COLORS.WHITE} fontSize="0.75rem">
+            Todos os direitos reservados.
+          </Text>
+          <Text textAlign="right" color={COLORS.WHITE} fontSize="0.75rem">
+            Studio Sol Books © 2023
+          </Text>
+        </Flex>
+      </Flex>
+    </Container>
+  </FooterTag>
+);
+
+const FooterTag = styled.footer`
+  width: 100%;
+  background-color: ${COLORS.PURPLE_100};
+  padding: 28px 0;
+  margin-top: 48px;
+`;
 
 export default Footer;
