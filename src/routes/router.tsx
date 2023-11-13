8b8cbd10e5daafe921c8ae8ba2beb752599a2551
roger067 <rosestolato.rogerio@gmail.com>
2023-11-13 20:22:22 -0300

    fix: create errorElement

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,14 +1,28 @@
 import { createBrowserRouter } from 'react-router-dom';
+
 import { BookList, BookDetail } from '../pages';
+import { ErrorMessage } from '../ui';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <BookList />,
+    errorElement: (
+      <ErrorMessage
+        title="Desculpe, essa página não existe!"
+        description="Volte para a página principal."
+      />
+    ),
   },
   {
     path: '/:id',
     element: <BookDetail />,
+    errorElement: (
+      <ErrorMessage
+        title="Desculpe, essa página não existe!"
+        description="Volte para a página principal."
+      />
+    ),
   },
 ]);
 
